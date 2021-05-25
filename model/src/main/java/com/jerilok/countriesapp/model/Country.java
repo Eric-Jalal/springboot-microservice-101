05614d77d49d0667b3c757fc5c65c619a1c63c72
Eric Jalal <eric.jalal@elisa.fi>
2021-05-25 22:07:08 +0300

    Add country dto model

---
+++ model/src/main/java/com/jerilok/countriesapp/model/Country.java
@@ -0,0 +1,80 @@
+package com.jerilok.countriesapp.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.net.URL;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Country {
+
+    @JsonProperty("name")
+    private String name;
+
+    @JsonProperty("country_code")
+    private String countryCode;
+
+    @JsonProperty("capital")
+    private String capital;
+
+    @JsonProperty("population")
+    private Integer population;
+
+    @JsonProperty("flag_file_url")
+    private URL flagFileUrl;
+
+    public Country() {}
+
+    public Country(String name, String countryCode) {
+        this.name = name;
+        this.countryCode = countryCode;
+    }
+
+    public Country(String name, String countryCode, String capital, Integer population, URL flagFileUrl) {
+        this.name = name;
+        this.countryCode = countryCode;
+        this.capital = capital;
+        this.population = population;
+        this.flagFileUrl = flagFileUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountryCode() {
+        return countryCode;
+    }
+
+    public void setCountryCode(String countryCode) {
+        this.countryCode = countryCode;
+    }
+
+    public String getCapital() {
+        return capital;
+    }
+
+    public void setCapital(String capital) {
+        this.capital = capital;
+    }
+
+    public Integer getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(Integer population) {
+        this.population = population;
+    }
+
+    public URL getFlagFileUrl() {
+        return flagFileUrl;
+    }
+
+    public void setFlagFileUrl(URL flagFileUrl) {
+        this.flagFileUrl = flagFileUrl;
+    }
+}
