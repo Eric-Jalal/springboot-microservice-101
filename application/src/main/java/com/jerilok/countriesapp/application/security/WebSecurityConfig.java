53a92458cd522edfa5e487076256ab0957cd8a15
Eric Jalal <eric.jalal@elisa.fi>
2021-05-26 11:14:17 +0300

    Enable websecurity config
    - Whitelist implemented endpoints
    - Ignore actuator endpoints from being authenticated

---
+++ application/src/main/java/com/jerilok/countriesapp/application/security/WebSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.jerilok.countriesapp.application.security;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        logger.info("Setting security configuration");
+
+        http.csrf().disable();
+
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.NEVER);
+
+        http.authorizeRequests()
+                .mvcMatchers("/countries")
+                .permitAll()
+                .and()
+                .authorizeRequests()
+                .anyRequest()
+                .authenticated();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/version", "/actuator/health");
+    }
+}
